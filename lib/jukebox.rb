8d7fb48b246e13a473712c092b73f5249280e540
github username <github email address>
2019-01-26 15:09:21 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,3 +1,4 @@
+require_relative
 songs = [
   "Phoenix - 1901",
   "Tokyo Police Club - Wait Up",
@@ -10,3 +11,66 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+# def say_hello(name)
+  # "Hi #{name}!"
+# end
+
+# puts "Enter your name:"
+# users_name = gets.chomp
+
+# puts say_hello(users_name)
+
+def help
+  puts "I accept the following commands:
+  - help : displays this help message
+  - list : displays a list of songs you can play
+  - play : lets you choose a song to play
+  - exit : exits this program"
+end
+
+def list(songs)
+  songs.each do |element|
+    puts "#{songs.index(element)+1}. #{element}"
+  end
+end
+
+
+def play(songs)
+  puts "Enter your song of choice:"
+  input = gets.downcase.chomp
+    songs.each do |element|
+      if songs.index(element)+1 == input.to_i
+        puts "Now Playing: #{element}"
+        elsif element.downcase == input
+          puts "Now Playing: #{element}"
+          else
+            puts "Invalid input, please try again"
+          end
+        end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+
+def run(songs)
+  help
+  puts "Please enter a command:"
+  input = gets.chomp
+  case input
+    when "list"
+      list(songs)
+    when "play"
+      list(songs)
+      play(songs)
+    when "help"
+      help
+    when "exit"
+      exit_jukebox
+    else
+      help
+    end
+end
+
+run()
